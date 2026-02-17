f33d7b2d9ff02850b07f281b43751570be8a3b58
sgavka <sgavka@gmail.com>
2026-02-18 01:41:02 +0200

    fix

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -27,6 +27,7 @@ def create_storage() -> Union[RedisStorage, MemoryStorage]:
             host=settings.REDIS_HOST,
             port=settings.REDIS_PORT,
             db=settings.REDIS_BOT_DB,
+            password=settings.REDIS_PASSWORD,
             decode_responses=True,
         )
         return RedisStorage(redis=redis)
